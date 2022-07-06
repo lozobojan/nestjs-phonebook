0671e70dd2c09972076d8612cf3a57e67646ec89
Bojan Lozo <bojanlo@maestralsolutions.com>
2022-07-06 12:15:59 +0200

    Added bcrypt package and password hashing method

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import {JwtService} from "@nestjs/jwt";
 import {User} from "../../user/models/user.entity";
 import {from, Observable} from "rxjs";
+const bcrypt = require('bcrypt');
 
 @Injectable()
 export class AuthService {
@@ -11,6 +12,10 @@ export class AuthService {
 
     generateJWT(user: User): Observable<string>{
         return from(this.jwtService.signAsync(user));
+    }
+
+    hashPassword(password: string): Observable<string>{
+        return from<string>(bcrypt.hash(password, 12));
     }
 
 
